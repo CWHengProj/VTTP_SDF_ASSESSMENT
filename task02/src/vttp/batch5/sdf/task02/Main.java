e2861fb4da174af57934cbc4956efbd210f0aa76
Choon Wee <142023227+CWHengProj@users.noreply.github.com>
2024-10-30 15:24:08 +0800

    able to produce a line output but bugs to fix

--- task02/src/vttp/batch5/sdf/task02/Main.java
+++ task02/src/vttp/batch5/sdf/task02/Main.java
@@ -3,6 +3,7 @@ package vttp.batch5.sdf.task02;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.List;
 
 import vttp.batch5.sdf.task02.supp.Helper;
 
@@ -19,10 +20,9 @@ public class Main {
 		String fileDir = "C:\\Users\\Admin\\Desktop\\vttp_b5_assessment_template\\task02\\" + cleanedInput;//TODO make the file path relative
 		System.out.printf("Processing: %s\n\n",args[0]);
 		char[][] gameGrid = helper.getGameGrid(fileDir);
-		helper.simulateMoves(gameGrid,true);
-		System.out.println(helper.getFinalEval());
-		//list of all legal positions for X, and its utility
-		// y=1 , x=0, utility=0
+		helper.simulateMoves(gameGrid);
+		
+		
 
 	}
 }
--- task02/src/vttp/batch5/sdf/task02/supp/Helper.java
+++ task02/src/vttp/batch5/sdf/task02/supp/Helper.java
@@ -9,11 +9,6 @@ import java.util.List;
 
 public class Helper {
     public int turnCount=0;
-    public List<List<Integer>> finalEval = new ArrayList<>();
-    
-    public List<List<Integer>> getFinalEval() {
-        return finalEval;
-    }
     public char[][] getGameGrid(String fileDir) throws IOException {
 		System.out.println("Board:");
 		//read the text files
@@ -39,113 +34,111 @@ public class Helper {
         return gameGrid;
     }
     public int scoreChecker(char[][] gameGrid) {
-        //check who won, print out the winner name and exit the application  
+        //check if player won
         for (int i=0; i<3; i++){
             if (gameGrid[i][0]==gameGrid[i][1] &&gameGrid[i][1]==gameGrid[i][2]){
-                if(gameGrid[i][0]== 'X' || gameGrid[i][0]=='O'){
-                    if (gameGrid[i][0]=='X'){
-                        return 1;
-                    }
-                    else{
-                        return -1;
-                    }
+                if(gameGrid[i][0]== 'X'){
+                    return 1;
                 }
             }
         }
         for (int j=0; j<3; j++){
             if (gameGrid[0][j]==gameGrid[1][j] &&gameGrid[1][j]==gameGrid[2][j]){
-                if(gameGrid[0][j]== 'X' || gameGrid[0][j]=='O'){
-                    if (gameGrid[0][j]=='X'){
-                        return 1;
-                    }
-                    else{
-                        return -1;
-                    }                }
-            } 
+                if(gameGrid[0][j]== 'X'){
+                    return 1;
+            } }
         }
         if (gameGrid[0][0]==gameGrid[1][1] &&gameGrid[1][1]==gameGrid[2][2]){
-            if(gameGrid[0][0]== 'X' || gameGrid[0][0]=='O'){
-                if (gameGrid[0][0]=='X'){
-                    return 1;
-                }
-                else{
-                    return -1;
-                }            
+            if(gameGrid[0][0]== 'X'){
+                return 1;   
             }
         }
         if (gameGrid[0][2]==gameGrid[1][1] &&gameGrid[1][1]==gameGrid[2][0]){
-            if(gameGrid[0][2]== 'X' || gameGrid[0][2]=='O'){
-                if (gameGrid[0][2]=='X'){
-                    return 1;
-                }
-                else{
-                    return -1;
-                }            }
+            if(gameGrid[0][2]== 'X'){
+                return 1;         
+            }
         } 
-        int emptyCells = 0; //check if current boardstate is end of game.
-        for (char[] c: gameGrid){
-            for (char x: c){
-                if (x=='.'){
-                    emptyCells++;
-                }
+        //cases where O wins in a move turn
+        char[][] tempGrid = gameGrid;
+
+        //check through all the win conditions again and return 0
+        for (int l=0; l<3; l++){
+            int oCount =0;
+            if (tempGrid[l][0]=='O'){
+                oCount++;
+            }
+            if (tempGrid[l][1]=='O'){
+                oCount++;
+            }
+            if (tempGrid[l][2]=='O'){
+                oCount++;
+            }
+            if (oCount>1){
+                return -1;
             }
         }
-        if (emptyCells==0){
-            return 0;
+        for (int k=0; k<3; k++){
+            int oCount=0;
+            if (tempGrid[0][k]=='O'){
+                oCount++;
+            }
+            if (tempGrid[1][k]=='O'){
+                oCount++;
+            }
+            if (tempGrid[2][k]=='O'){
+                oCount++;
+            }
+            if (oCount>1){
+                return -1;
+            }
+
         }
-        if (turnCount==1){
-            //if player turn has already be simulated and inconclusive, return 0 
-            return 0;
+        int dCount=0;
+        if (tempGrid[0][0]=='O'){
+            dCount++;
         }
-        else
-            turnCount++;
-            return simulateMoves(gameGrid,false); //Check the next state and its possibilities, return utility
-    }
-    public int simulateMoves(char[][] gameGrid, boolean isPlayerTurn) {
-        int bestScore = -12222222;
-        int worstScore = 12222222;
-        char playerToken;
-        if (isPlayerTurn){
-            playerToken = 'X';
+        if (tempGrid[1][1]=='O'){
+            dCount++;
         }
-        else
-            playerToken='O';
+        if (tempGrid[2][2]=='O'){
+            dCount++;
+        }
+        if (dCount>1){
+            return -1;
+        }
+        dCount=0;
+        if (tempGrid[0][2]=='O'){
+            dCount++;
+        }
+        if (tempGrid[1][1]=='O'){
+            dCount++;
+        }
+        if (tempGrid[2][0]=='O'){
+            dCount++;
+        }
+        if (dCount>1){
+            return -1;
+        }
+        return 0;
+
+    } 
+    public void simulateMoves(char[][] gameGrid) {
         char[][] simGrid = gameGrid;
         //for each empty cell, try and check the win state.
-        List<Integer> innerList = new ArrayList<>();
         //TODO add each state to a list where it can be returned
         for (int i=0;i<3;i++){
             for (int j=0; j<3; j++){
                 if(simGrid[i][j]=='.'){
-                    simGrid[i][j]= playerToken;
+                    simGrid[i][j]= 'X';
                     //simulate each position and map to a list the returned score
-                    if (isPlayerTurn){
-                        if (scoreChecker(simGrid)>bestScore){
-                            bestScore= scoreChecker(simGrid);
-                            innerList.add(j);
-                            innerList.add(i);
-                            innerList.add(bestScore);
-                            finalEval.add(innerList);
-                        }
-                    }
-                    else{
-                        if (scoreChecker(simGrid)<worstScore){
-                            worstScore= scoreChecker(simGrid);
-                        }
-                    }
+                    System.out.printf("y=%d, ",j);
+                    System.out.printf("x=%d, ",i);
+                    int returnedValue = scoreChecker(simGrid);
+                    System.out.printf("utility=%d\n",returnedValue);
                     simGrid[i][j]='.';//undo board state
                 }
             }
         }
-        if (isPlayerTurn){
-            return bestScore;
-        }
-        else{
-
-            return bestScore;
-        }
-
-
     }
     
 }
